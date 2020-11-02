2612bafba6fa9bb709e94b844047117d143e3acd
joaocairiton <43797301+joaocairiton@users.noreply.github.com>
2020-11-01 22:01:46 -0300

    Calculadora Simples

---
+++ Calculadora2/src/calculadora2/Calculadora2.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora2;
+
+import javax.swing.UIManager;
+
+/**
+ *
+ * @author joaoc
+ */
+public class Calculadora2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        TelaCalculadora t = new TelaCalculadora();
+        t.setVisible(true);
+    }
+    
+    
+
+}
